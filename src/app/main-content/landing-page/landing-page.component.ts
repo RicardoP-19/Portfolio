a0940f8d525326593b4e731732cee58745e1f753
Ricardo <ricardo-pinto@web.de>
2025-01-17 22:37:07 +0100

    animation_of_headline

--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  hoverIndexTop: number | null = null;
+  hoverIndexBottom: number | null = null;
 
+  headlineText = [  
+    {
+      noHoverFrontend: ['F', 'r', 'o', 'n', 't', 'e', 'n', 'd'],
+      hoverFrontend: ['f', 'R', 'O', 'N', 'T', 'E', 'N', 'D'],
+      noHoverDeveloper: ['D', 'E', 'V', 'E', 'L', 'O', 'P', 'E', 'R'],
+      hoverDeveloper: ['d', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+    } 
+  ]
+
+  mouseOverTop(index: number) {
+    this.hoverIndexTop = index;
+  }
+
+  mouseOutTop() {
+    this.hoverIndexTop = null;
+  }
+
+  mouseOverBottom(index: number) {
+    this.hoverIndexBottom = index;
+  }
+
+  mouseOutBottom() {
+    this.hoverIndexBottom = null;
+  }
 }
