8de79032e1de220e5935d1c893151318bb562fee
Ricardo <ricardo-pinto@web.de>
2025-02-11 23:30:01 +0100

    add_imprint_and_privacy_policy

---
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-privacy-policy',
+  standalone: true,
+  imports: [TranslateModule, CommonModule],
+  templateUrl: './privacy-policy.component.html',
+  styleUrl: './privacy-policy.component.scss'
+})
+export class PrivacyPolicyComponent {
+
+  constructor(public translate: TranslateService) {}
+
+}
