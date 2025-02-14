b3a8817ace68f366e7d02eb170695d11c4b8f731
Ricardo <ricardo-pinto@web.de>
2025-02-14 14:08:31 +0100

    change_imprint_style

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { HeaderComponent } from "../shared/components/header/header.component";
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, HeaderComponent, TranslateModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
 
+  constructor(public translate: TranslateService) {}
 }
