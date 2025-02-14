52a5eedbe7272b94c8905d2c32a472fcaed4946b
Ricardo <ricardo-pinto@web.de>
2025-02-14 23:19:31 +0100

    language_service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { IntroComponent } from './intro/intro.component';
 import * as AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -10,20 +9,14 @@ import 'aos/dist/aos.css';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, IntroComponent, HeaderComponent, TranslateModule],
+  imports: [CommonModule, RouterOutlet, IntroComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent implements OnInit{
   title = 'portfolio';
   showIntro: boolean = true;
-  hideHeader: boolean = false;
-
-  constructor(private translate: TranslateService, private router: Router) {
-    this.translate.addLangs(['de', 'en']);
-    this.translate.setDefaultLang('en');
-    this.translate.use('en');
-  }
 
   ngOnInit():void {
     if (sessionStorage.getItem('introShown')) {
@@ -38,12 +31,6 @@ export class AppComponent implements OnInit{
     AOS.init({
       easing: 'ease-in-out',
       once: true,
-    });
-
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.hideHeader = event.url === '/imprint' || event.url === '/privacy-policy';
-      } 
     });
   }
 }
