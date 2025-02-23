218e2e645fb8ea12a2f374b76ea509b1110a647a
Ricardo <ricardo-pinto@web.de>
2025-02-23 17:57:41 +0100

    send_translate

--- src/app/main-content/contact-me/send-confirm/send-confirm.component.ts
+++ src/app/main-content/contact-me/send-confirm/send-confirm.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-send-confirm',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './send-confirm.component.html',
   styleUrl: './send-confirm.component.scss'
 })
 export class SendConfirmComponent {
   @Input() sendMail: boolean = false;
+
+  constructor(public translate: TranslateService) {}
 }
